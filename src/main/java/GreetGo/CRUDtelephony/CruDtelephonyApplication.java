15afced8fa198c490663d7c128c6aa36e76211b4
abaiA <abai.45.abai@gmail.com>
2024-06-11 20:44:41 +0500

    first commit

---
+++ src/main/java/GreetGo/CRUDtelephony/CruDtelephonyApplication.java
@@ -0,0 +1,13 @@
+package GreetGo.CRUDtelephony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDtelephonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDtelephonyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GreetGo/CRUDtelephony/CruDtelephonyApplicationTests.java
@@ -0,0 +1,13 @@
+package GreetGo.CRUDtelephony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDtelephonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
